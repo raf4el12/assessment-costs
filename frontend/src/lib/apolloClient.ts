cc639cd219cfb5fa6fd8ee417651fd4cf6516eb5
raf4el12 <gomerorafael121@gmail.com>
2026-02-10 20:15:03 -0500

    chore: limpieza de proyecto para iniciar arquitectura desde cero

--- frontend/src/lib/apolloClient.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client/core";
-
-export const apolloClient = new ApolloClient({
-  link: new HttpLink({
-    uri: "http://localhost:4000/",
-  }),
-  cache: new InMemoryCache(),
-});
